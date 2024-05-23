082488179b72c2b72f4d3e2956bcfcbee0e965e0
GudalaSrihari9993 <srihari9993@gmail.com>
2024-05-23 18:26:49 +0530

    after adding evenodd function

--- array.c
+++ array.c
@@ -1,4 +1,19 @@
 #include<stdio.h>
+void evenodd(int *arr , int n)
+{
+      int i;
+      for(i=0;i<n;i++)
+      {
+          if(arr[i]%2==0)
+          {
+             printf("even\n");
+          }
+          else
+          {
+            printf("odd\n");
+          }
+      }
+}
 int main()
 {
      int arr[100],n,res;
@@ -8,6 +23,7 @@ int main()
      {
         scanf("%d",&arr[i]);
      }
+     evenodd(arr,n);
      
 
      
