8fdfa41fa311e79a3e0bca5f279459ddb4edba49
GudalaSrihari9993 <srihari9993@gmail.com>
2024-05-23 17:08:40 +0530

    adding arrayc to master branch

---
+++ array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+     int arr[100],n,res;
+     printf("enter the number of elements:");
+     scanf("%d",&n);
+     for(int i;i<n;i++)
+     {
+        scanf("%d",&arr[i]);
+     }
+     
+
+     
+}
