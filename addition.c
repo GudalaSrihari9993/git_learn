6997f24efc5bc362c47b77de7e020b2d69f5fa1c
GudalaSrihari9993 <srihari9993@gmail.com>
2024-05-27 19:27:22 +0530

    addition.c added

---
+++ addition.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+     int a,b,c;
+     printf("enter the numbers:");
+     scanf("%d%d",&a,&b);
+     c=a+b;
+     printf("addition of the two numbers is %d\n",c);
+}
